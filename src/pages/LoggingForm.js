5b65d7774ca6ee7774cdd4eda61bd8f7205aa0df
Justyna Brzezinska <brzezinska.j.uczelnia@gmail.com>
2020-10-12 19:39:10 +0200

    page to logon without js

--- src/pages/LoggingForm.js
+++ src/pages/LoggingForm.js
@@ -1,29 +1,31 @@
 import React from 'react'
 import logo from '../assets/logo.png'
-import {MDBIcon} from 'mdbreact'
+import { MDBIcon } from 'mdbreact'
 
 class LoggingForm extends React.Component {
   render () {
     return (
-      //   <div className='grid-row logging-form'>
-      //     <div className='grid-row--col-1-of-2'>logon page</div>
-      //     <div className='grid-row--col-1-of-2'>here logo</div>
-      //   </div>
-
       <div className=' logging-form u-center-text'>
+      <form>
         <img className='logging-form__logo' src={logo} alt='logo' />
         <div>
-        <div className='logging-form__input'>
-          <input className='logging-form__input--data' placeholder='email' />
+          <div className='logging-form__input'>
+            <input className='logging-form__input--data' type='email' placeholder='email' />
+          </div>
+          <div>
+            <input className='logging-form__input--data' type='password' placeholder='hasło' />
+          </div>
+          <button className='button button-red button-info button-submit logging-form__input--button' type='submit'>
+            zaloguj
+          </button>
+          <div className='logging-form__forgotten-pwd'>
+            zapomniałeś hasła?
+            <a href='!#' className='logging-form__forgotten-pwd--link'>
+              &nbsp; przypomnij
+            </a>
+          </div>
         </div>
-        <div>
-           <input className='logging-form__input--data' placeholder='hasło'/>
-        </div>
-         <button  className='button button-red button-info button-submit logging-form__input--button'>
-              zaloguj
-            </button>
-        </div>
-        
+        </form>
       </div>
     )
   }
